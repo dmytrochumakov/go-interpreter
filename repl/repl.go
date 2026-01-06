3b6fa0f7f2f56ae236a1f4caa5993d157667fef2
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2026-01-06 17:02:34 +0200

    feat: added bindings & the enviroment

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/dmytrochumakov/go-interpreter/evaluator"
 	"github.com/dmytrochumakov/go-interpreter/lexer"
+	"github.com/dmytrochumakov/go-interpreter/object"
 	"github.com/dmytrochumakov/go-interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnviroment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
